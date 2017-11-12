63206cf738bc828d5903349dbe878ab894051325
Augusto Lizarraga <augusjoaliza@hotmail.com>
2017-11-12 17:06:37 -0300

    Agrego clase Quini6

---
+++ src/fiuba/algo3/modelo/Quini6.java
@@ -0,0 +1,27 @@
+package fiuba.algo3.modelo;
+
+import java.util.Hashtable;
+
+public class Quini6 {
+	private Hashtable<Jugador, Integer> jugadores = new Hashtable<Jugador, Integer>();
+	private double primerPremio = 50000;
+	private double segundoPremio = 30000;
+	
+	public void visitar(Jugador jugador) {
+		if (!this.jugadores.containsKey(jugador)) {
+			jugador.agregarDinero(this.getPremio(0));
+			this.jugadores.put(jugador, 1);
+		}
+		else {
+			jugador.agregarDinero(this.getPremio(this.jugadores.get(jugador)));
+			this.jugadores.replace(jugador, 2);
+		}
+	}
+	
+	private double getPremio(int cantidaddeVisitas) {
+		if(cantidaddeVisitas == 0) return this.primerPremio;
+		if(cantidaddeVisitas == 1) return this.segundoPremio;
+		return 0;
+	}
+
+}
---
+++ test/fiuba/algo3/modelo/Quini6Test.java
@@ -0,0 +1,42 @@
+package fiuba.algo3.modelo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Quini6Test {
+	private static final double DELTA = 1e-15;
+	
+	@Test
+	public void testCrearQuini6(){
+		Quini6 quini6 = new Quini6();
+		Assert.assertNotNull(quini6);
+	}
+
+	@Test
+	public void testJugadorVisitaQuini6unaVez() {
+		Quini6 quini6 = new Quini6();
+		Jugador jugador = new Jugador(100000);
+		quini6.visitar(jugador);
+		Assert.assertEquals(150000, jugador.getDinero(), DELTA);
+	}
+	
+	@Test
+	public void testJugadorVisitaQuini6DosVeces() {
+		Quini6 quini6 = new Quini6();
+		Jugador jugador = new Jugador(100000);
+		quini6.visitar(jugador);
+		quini6.visitar(jugador);
+		Assert.assertEquals(180000, jugador.getDinero(), DELTA);
+	}
+	
+	@Test
+	public void testJugadorVisitaQuini6TresVeces() {
+		Quini6 quini6 = new Quini6();
+		Jugador jugador = new Jugador(100000);
+		quini6.visitar(jugador);
+		quini6.visitar(jugador);
+		quini6.visitar(jugador);
+		Assert.assertEquals(180000, jugador.getDinero(), DELTA);
+	}
+	
+}
