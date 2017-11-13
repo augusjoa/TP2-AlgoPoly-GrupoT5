932c63eb6f8c8c0d84afe7fb81a68cecd54d968c
Martin Brandstetter <martinbran94@gmail.com>
2017-11-13 17:58:10 -0300

    carcel v2 incompleto
    en la fiuba los 3 integrantes

--- src/fiuba/algo3/modelo/Carcel.java
+++ src/fiuba/algo3/modelo/Carcel.java
@@ -8,18 +8,24 @@ public class Carcel {
 	public Hashtable<Jugador, Integer> jugadoresEnCarcel = new Hashtable<Jugador, Integer>();
 	
 	public void retenerJugador(Jugador jugador){
+		if(this.jugadoresEnCarcel.containsKey(jugador)) {
+			int num = jugadoresEnCarcel.get(jugador);
+			jugadoresEnCarcel.replace(jugador, num+1);
+			
+		}
 		if(!this.jugadoresEnCarcel.containsKey(jugador)){
 			this.jugadoresEnCarcel.put(jugador, 1);
 			jugador.setAccionDeMovimiento(false);
 			
 		}
-		else if(jugadoresEnCarcel.get(jugador)==2 && jugadoresEnCarcel.get(jugador)==3){
-			//jugador puede pagar la fianza
-			//vacior jugador del dic
+		else if(jugadoresEnCarcel.get(jugador) == 2 && jugadoresEnCarcel.get(jugador) == 3){
+			//paga si tiene plata
+			jugadoresEnCarcel.remove(jugador);
+			
 		}
-		else if(jugadoresEnCarcel.get(jugador)==4){
+		else if(jugadoresEnCarcel.get(jugador) == 4){
 			jugador.setAccionDeMovimiento(true);
-			//vaciar jugador del dic
+			jugadoresEnCarcel.remove(jugador);
 		}
 		//jugador.pasarTurno();
 	}
