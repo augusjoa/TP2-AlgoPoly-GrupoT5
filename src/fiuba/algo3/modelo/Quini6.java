682c855a407fc5a7fa0fc9c6e2da0c34734e0c0e
costagonzalo <costagonzalo96@gmail.com>
2017-11-12 21:57:45 -0300

    fin refactor quini6

--- src/fiuba/algo3/modelo/Quini6.java
+++ src/fiuba/algo3/modelo/Quini6.java
@@ -7,29 +7,12 @@ public class Quini6 {
 	private double primerPremio = 50000;
 	private double segundoPremio = 30000;
 	
-	/*public void visitar(Jugador jugador) {
-		if (!this.jugadores.containsKey(jugador)) {
-			jugador.agregarDinero(this.getPremio(0));
-			this.jugadores.put(jugador, 1);
-		}
-		else {
-			jugador.agregarDinero(this.getPremio(this.jugadores.get(jugador)));
-			this.jugadores.replace(jugador, 2);
-		}
-	}
-	
-	private double getPremio(int cantidaddeVisitas) {
-		if(cantidaddeVisitas == 0) return this.primerPremio;
-		if(cantidaddeVisitas == 1) return this.segundoPremio;
-		return 0;
-	}*/
-	
 	public void visitar(Jugador jugador){
 		if(!this.jugadores.containsKey(jugador)){
 			jugador.agregarDinero(primerPremio);
 			this.jugadores.put(jugador, 1);
 		}
-		if(jugadores.get(jugador)==1){
+		else if(jugadores.get(jugador)==1){
 			jugador.agregarDinero(segundoPremio);
 			this.jugadores.replace(jugador, 2);
 		}
