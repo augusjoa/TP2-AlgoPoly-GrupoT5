5729271dbd358cece942c3032e4264723a290c38
Martin Brandstetter <martinbran94@gmail.com>
2017-11-14 00:24:34 -0300

    Lo basico de Barrio
    solo atributo propietario

---
+++ src/fiuba/algo3/modelo/Barrio.java
@@ -0,0 +1,15 @@
+package fiuba.algo3.modelo;
+
+public class Barrio {
+
+	private Jugador propietario;
+
+	public Jugador setPropietario(Jugador unJugador) {
+		return propietario = unJugador;
+	}
+
+	public Jugador getPropietario() {
+		return propietario;
+	}
+
+}
---
+++ test/fiuba/algo3/modelo/BarrioTest.java
@@ -0,0 +1,19 @@
+package fiuba.algo3.modelo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BarrioTest {
+private static final double DELTA = 1e-15;
+	
+	@Test
+	public void testAlComprarElBarrioElJugadorEsPropietario() {
+		Barrio unBarrio = new Barrio();
+		Jugador unJugador = new Jugador(100000);
+		
+		unBarrio.setPropietario(unJugador);
+		
+		Assert.assertEquals(unJugador, unBarrio.getPropietario());
+	}
+	
+}
