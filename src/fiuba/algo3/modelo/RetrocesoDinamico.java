d709457b8e5908983941c1c717a44c86871e826f
Martin Brandstetter <martinbran94@gmail.com>
2017-11-14 01:38:48 -0300

    RetrocesoDinamico
    Codigo duplicado, posible refactorizacion.

---
+++ src/fiuba/algo3/modelo/RetrocesoDinamico.java
@@ -0,0 +1,17 @@
+package fiuba.algo3.modelo;
+
+public class RetrocesoDinamico {
+
+	public Jugador avanzar(Jugador unJugador, int numeroDelDado) {
+		
+		if(numeroDelDado < 11) {
+			unJugador.moverse((int) (unJugador.getDinero() / numeroDelDado));
+			return unJugador;
+		}
+		
+		unJugador.moverse(numeroDelDado - 2);
+		
+		return unJugador;
+	}
+
+}
---
+++ test/fiuba/algo3/modelo/RetrocesoDinamicoTest.java
@@ -0,0 +1,32 @@
+package fiuba.algo3.modelo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RetrocesoDinamicoTest {
+	@Test
+	public void testCaerEnLaCasillaCon7Hasta10(){
+		Jugador unJugador = new Jugador(9*25+8);		
+		RetrocesoDinamico unRetrocesoDinamico = new RetrocesoDinamico();
+		int posicionInicial = unJugador.posicion;
+		
+		unRetrocesoDinamico.avanzar(unJugador, 9);
+		
+		Assert.assertEquals(posicionInicial + 25, unJugador.posicion);
+	}
+	
+	@Test
+	public void testCaerEnLaCasillaCon11oMas(){
+		Jugador unJugador = new Jugador(100000);		
+		RetrocesoDinamico unRetrocesoDinamico = new RetrocesoDinamico();
+		int posicionInicial = unJugador.posicion;
+		
+		unRetrocesoDinamico.avanzar(unJugador, 11);
+		
+		Assert.assertEquals(posicionInicial + 9, unJugador.posicion);
+	}
+	
+	//Codigo duplicado en AvanceDinamico
+	//El tercer comportamiento en ambos casos es mas complicado, el Jugador debería almacenar una 
+	//lista de propiedades adquiridas, en contra de lo que dijimos de que lo almacene el barrio su propietario
+}
