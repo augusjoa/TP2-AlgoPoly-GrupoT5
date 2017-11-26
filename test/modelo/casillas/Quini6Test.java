761f50ad2a4db6807aae106fbd0e0a44185e4170
Unknown <martinbran94@gmail.com>
2017-11-26 18:40:57 -0300

    Quini6 ahora compara dinero en lugar de int

--- test/modelo/casillas/Quini6Test.java
+++ test/modelo/casillas/Quini6Test.java
@@ -3,11 +3,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import modelo.casillas.Quini6;
+import modelo.Dinero;
 import modelo.Jugador;
 
 
 public class Quini6Test {
-	//private static final double DELTA = 1e-15;
+	private static final int DINEROINICIAL = 100000;
 	
 	@Test
 	public void testCrearQuini6(){
@@ -19,27 +20,30 @@ public class Quini6Test {
 	public void testJugadorVisitaQuini6unaVez() {
 		Quini6 quini6 = new Quini6();
 		Jugador unJugador = new Jugador();
+		Dinero dineroFinal = new Dinero (DINEROINICIAL + 50000);
 		quini6.esVisitadoPorJugador(unJugador);
-		Assert.assertEquals(150000, unJugador.getDinero());
+		Assert.assertEquals(dineroFinal, unJugador.getDinero());
 	}
 	
 	@Test
 	public void testJugadorVisitaQuini6DosVeces() {
 		Quini6 quini6 = new Quini6();
 		Jugador jugador = new Jugador();
+		Dinero dineroFinal = new Dinero (DINEROINICIAL + 50000 + 30000);
 		quini6.esVisitadoPorJugador(jugador);
 		quini6.esVisitadoPorJugador(jugador);
-		Assert.assertEquals(180000, jugador.getDinero());
+		Assert.assertEquals(dineroFinal, jugador.getDinero());
 	}
 	
 	@Test
 	public void testJugadorVisitaQuini6TresVeces() {
 		Quini6 quini6 = new Quini6();
 		Jugador jugador = new Jugador();
+		Dinero dineroFinal = new Dinero (DINEROINICIAL + 50000 + 30000 + 0);
 		quini6.esVisitadoPorJugador(jugador);
 		quini6.esVisitadoPorJugador(jugador);
 		quini6.esVisitadoPorJugador(jugador);
-		Assert.assertEquals(180000, jugador.getDinero());
+		Assert.assertEquals(dineroFinal, jugador.getDinero());
 	}
 	
 }
