b0e83e0dd64fb159001273de3e39bef024a69a17
Augusto Lizarraga <augusjoaliza@hotmail.com>
2017-11-27 22:42:58 -0300

    Corrijo Carcel

--- src/modelo/casillas/Carcel.java
+++ src/modelo/casillas/Carcel.java
@@ -7,43 +7,19 @@ import modelo.Jugador;
 import modelo.interfaces.Visitable;
 
 public class Carcel implements Visitable{
-	private Hashtable<Jugador, Integer> jugadoresEnCarcel = new Hashtable<Jugador, Integer>();
 	private Dinero valorDeLaFianza = new Dinero(45000);
 	
-	public boolean jugadorEstaEnCarcel(Jugador jugador){
-		return this.jugadoresEnCarcel.containsKey(jugador);
-	}
-	
-	public boolean incrementarTurno(Jugador jugador){
-		if(jugadorEstaEnCarcel(jugador)) {
-			int num = jugadoresEnCarcel.get(jugador);
-			jugadoresEnCarcel.replace(jugador, num+1);
-			return true;
-		}
-		return false;
-	}
-	
-	public void retenerJugador(Jugador jugador){
-		jugadoresEnCarcel.put(jugador, 1);
-		jugador.jugadorDetenido();	
-	}
-	
+
 	public void pagarFianza(Jugador jugador){
-		//if(jugadoresEnCarcel.get(jugador) == 2 || jugadoresEnCarcel.get(jugador) == 3){
+		
 			jugador.getDinero().sustraerDinero(valorDeLaFianza);
-			liberarJugador(jugador);
-		//}
+			jugador.liberar();
+
 	}
 	
-	public void liberarJugador(Jugador jugador){
-		jugadoresEnCarcel.remove(jugador);
-		jugador.jugadorLiberado();
-
-	}
-
 	@Override
 	public void esVisitadoPorJugador(Jugador jugador) {
-		if(!jugadoresEnCarcel.containsKey(jugador))	retenerJugador(jugador);
+		jugador.encarcelar();
 	}
 	
 }
