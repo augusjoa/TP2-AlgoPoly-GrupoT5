05c57029b0d70b3d014d08e060a7b88e8fffda42
costagonzalo <costagonzalo96@gmail.com>
2017-11-30 14:46:52 -0300

    Arreglo codigo alertas

--- src/vista/eventos/MoverseOnAction.java
+++ src/vista/eventos/MoverseOnAction.java
@@ -11,7 +11,6 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import modelo.AlgoPoly;
 import modelo.casillas.AvanceDinamico;
 import modelo.casillas.ImpuestoAlLujo;
@@ -41,7 +40,6 @@ public class MoverseOnAction implements EventHandler<ActionEvent> {
 		
 		alertaAlCaerEnAvance();
 		alertaAlCaerEnRetroceso();
-		
 		alertaAlCaerEnCarcel();
 		alertaAlCaerEnQuini6();
 		alertaAlCaerEnImpuesto();
@@ -49,17 +47,26 @@ public class MoverseOnAction implements EventHandler<ActionEvent> {
 		
 		juego.update();
 	}
+	
+	public void crearAlerta(String nombre,String texto){
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle(nombre);
+    	alert.setHeaderText("");
+    	alert.setContentText(texto);
+    	Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+    	stage.setOnCloseRequest(e->e.consume());
+    	stage.getIcons().add(new Image(getClass().getResourceAsStream("../img/icon.png")));
+    	Optional<ButtonType> result = alert.showAndWait();
+    	if (result.get() == ButtonType.OK){
+    		juego.updateDinero();
+    	}
+	}
 
 	private void alertaAlCaerEnCarcel() {
 		Visitable casillaActual= partida.getJugadorActual().getCasillaActual();
 		if(partida.getJugadorActual().getCasillaActual() == partida.tablero().getCarcel()){
-			Alert alert = new Alert(AlertType.INFORMATION);
-			alert.setTitle(casillaActual.getNombre());
-	    	alert.setHeaderText("");
-	    	alert.setContentText("Se encuentra atrapado en la carcel, deberá pagar una fianza en los proximos turnos o esperar a ser liberado");
-	    	Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
-	    	stage.getIcons().add(new Image(getClass().getResourceAsStream("../img/icon.png")));
-	    	alert.showAndWait();
+			String texto= "Se encuentra atrapado en la carcel, deberá pagar una fianza en los proximos turnos o esperar a ser liberado";
+			crearAlerta(casillaActual.getNombre(),texto);
 		}
 	}
 	
@@ -88,45 +95,24 @@ public class MoverseOnAction implements EventHandler<ActionEvent> {
 	private void alertaAlCaerEnRetroceso(){
 		Visitable casillaActual= partida.getJugadorActual().getCasillaActual();
 		if( casillaActual == partida.tablero().getCasillero(18)){
-			Alert alert = new Alert(AlertType.INFORMATION);
-			alert.setTitle(casillaActual.getNombre());
-	    	alert.setHeaderText("");
-	    	alert.setContentText("Retrocedes: "+Integer.toString(((RetrocesoDinamico) casillaActual).getCantidadDePasos()));
-	    	Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
-	    	stage.setOnCloseRequest(e->e.consume());
-	    	stage.getIcons().add(new Image(getClass().getResourceAsStream("../img/icon.png")));
-	    	alert.showAndWait();
+			String texto= "Retrocedes: "+Integer.toString(((RetrocesoDinamico) casillaActual).getCantidadDePasos());
+			crearAlerta(casillaActual.getNombre(),texto);
 		}
 	}
 	
 	private void alertaAlCaerEnAvance(){
 		Visitable casillaActual= partida.getJugadorActual().getCasillaActual();
 		if( casillaActual == partida.tablero().getCasillero(7)){
-			Alert alert = new Alert(AlertType.INFORMATION);
-			alert.setTitle(casillaActual.getNombre());
-	    	alert.setHeaderText("");
-	    	alert.setContentText("Avanzas: "+Integer.toString(((AvanceDinamico) casillaActual).getCantidadDePasos()));
-	    	Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
-	    	stage.setOnCloseRequest(e->e.consume());
-	    	stage.getIcons().add(new Image(getClass().getResourceAsStream("../img/icon.png")));
-	    	alert.showAndWait();
+			String texto="Avanzas: "+Integer.toString(((AvanceDinamico) casillaActual).getCantidadDePasos());
+			crearAlerta(casillaActual.getNombre(),texto);
 		}
 	}
 	
 	private void alertaAlCaerEnImpuesto(){
 		Visitable casillaActual= partida.getJugadorActual().getCasillaActual();
 		if( casillaActual == partida.tablero().getCasillero(10)){
-			Alert alert = new Alert(AlertType.INFORMATION);
-			alert.setTitle(casillaActual.getNombre());
-	    	alert.setHeaderText("");
-	    	alert.setContentText("Perdes un "+Integer.toString(((ImpuestoAlLujo) casillaActual).getPorcentajeDelImpuesto())+"% de tu Dinero.");
-	    	Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
-	    	stage.setOnCloseRequest(e->e.consume());
-	    	stage.getIcons().add(new Image(getClass().getResourceAsStream("../img/icon.png")));
-	    	Optional<ButtonType> result = alert.showAndWait();
-	    	if (result.get() == ButtonType.OK || result.get() == ButtonType.CLOSE){
-	    		juego.updateDinero();
-	    	}
+			String texto= "Perdes un "+Integer.toString(((ImpuestoAlLujo) casillaActual).getPorcentajeDelImpuesto())+"% de tu Dinero.";
+			crearAlerta(casillaActual.getNombre(),texto);
 		}
 	}
 
